bc4c973d06f0d7caf49ea5895f77bae6d6c8e69e
Santoshsahajrao <santoshsahajrao71@gmail.com>
2017-12-02 20:14:48 +0530

    Wallpare Demo Completed

--- app/src/main/java/com/example/ravi/changewallpaper/MainActivity.java
+++ app/src/main/java/com/example/ravi/changewallpaper/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.ravi.changewallpaper;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.WallpaperManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.AudioManager;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -20,15 +27,17 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnChange,btnMobileData;
+    Button btnChange,btnMobileData,btnRingChange;
     Bitmap bitmap;
     SeekBar seekBar;
     TextView textView;
 
+    Ringtone ringtone;
     int count;
     AudioManager audioManager;
 
@@ -43,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
         btnChange = findViewById(R.id.btnChange);
         seekBar = findViewById(R.id.seek);
         textView =findViewById(R.id.txtmsg);
+        btnRingChange = findViewById(R.id.btnRing);
+
+
+
 
 
     //    audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
@@ -108,6 +121,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnRingChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+//                Intent intent = new Intent();
+//                intent.setType("audio/*");
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(intent,"Re"),66);
+
+                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                startActivityForResult(intent,66);
+
+
+            }
+        });
+
+
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+
+        }
+
+
 
     }
 
@@ -135,5 +176,83 @@ public class MainActivity extends AppCompatActivity {
                 }
 
         }
+
+        if (requestCode ==2)
+        {
+
+          Uri uri = data.getData();
+
+            Toast.makeText(this, " "+uri.toString(), Toast.LENGTH_SHORT).show();
+
+                String s = uri.getPath();
+//
+
+
+            File k = new File(s);
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DATA,k.getAbsolutePath());
+            contentValues.put(MediaStore.MediaColumns.TITLE,"ring");
+            contentValues.put(MediaStore.MediaColumns.SIZE,k.length());
+            contentValues.put(MediaStore.Audio.Media.ARTIST,R.string.app_name);
+            contentValues.put(MediaStore.Audio.Media.IS_RINGTONE,true);
+
+
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE,"audio/mp3");
+
+
+
+
+
+
+            Uri uri1 = MediaStore.Audio.Media.getContentUriForPath(k.getAbsolutePath());
+     getContentResolver().delete(uri,MediaStore.MediaColumns.DATA+ "=\"",null);
+
+
+            Uri uri2 = this.getContentResolver().insert(uri1,contentValues);
+
+
+
+
+
+settingPermision();
+
+                RingtoneManager.setActualDefaultRingtoneUri(MainActivity.this,RingtoneManager.TYPE_RINGTONE,uri2);
+                Toast.makeText(this, "Completed", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+
+
+        if(requestCode ==66)
+        {
+
+
+
+            Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+            settingPermision();
+            RingtoneManager.setActualDefaultRingtoneUri(MainActivity.this,RingtoneManager.TYPE_RINGTONE,uri);
+
+        }
+
+    }
+
+
+
+    public void settingPermision()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+
+            if (!Settings.System.canWrite(getApplicationContext()))
+            {
+
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,Uri.parse("package:"+getPackageName()));
+                startActivityForResult(intent,200);
+
+            }
+        }
+
+
     }
 }
